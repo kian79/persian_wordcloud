aed6a5a5107dbe7a36f83e9c18cf00c40b5ac0b8
kian79 <kiankr79@gmail.com>
2020-03-01 03:59:28 +0330

    No need to enter path to save the photo!

--- telegram_wordcloud.py
+++ telegram_wordcloud.py
@@ -1,12 +1,5 @@
+import os
+
 from bs4 import BeautifulSoup as Soup
 import re, urllib.request, nltk
-
-url = 'http://google.com'
-html = urllib.request.urlopen(url).read()  # make the request to the url
-soup = Soup(html)  # using Soup on the responde read
-for script in soup(["script", "style"]):  # You need to extract this <script> and <style> tags
-    script.extract()  # strip them off
-text = soup.getText()  # this is the method that I had like 40 min problems
-text = text.encode('utf-8')  # make sure to encode your text to be compatible
-# raw = nltk.clean_html(document)
-print(text)
+print(os.getcwd())
